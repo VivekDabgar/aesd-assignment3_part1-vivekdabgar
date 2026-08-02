cb95e3e76f954253259e2ce5044e3590eba53e78
VivekDabgar <vv.dabgar786@gmail.com>
2026-08-02 07:29:59 +0200

    Add aesdsocket start stop init script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Start/stop script for the aesdsocket daemon
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon -K -n aesdsocket --signal TERM
+        ;;
+    restart)
+        "$0" stop
+        sleep 1
+        "$0" start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
